ccd8d14ea872967581d562e9559d254fb7729b39
Eliot <eliot@launchcode.org>
2016-06-08 17:56:41 -0500

    working player

---
+++ Player.elm
@@ -0,0 +1,139 @@
+module Main exposing (..)
+
+import Html exposing (Html)
+import Html.Attributes as Attr
+import Html.Events as Events
+import Html.App as Html
+import Json.Decode as JsonD
+import Array exposing (Array)
+import Debug
+
+
+main : Program Never
+main =
+    Html.beginnerProgram
+        { model = model
+        , view = view
+        , update = update
+        }
+
+
+
+-- MODEL
+
+
+type alias Model =
+    { currentSong : Int
+    , files : List FileRecord
+    , queue : Array FileRecord
+    }
+
+type alias FileRecord =
+    { path : String
+    , name : String
+    }
+
+
+model : Model
+model =
+    { currentSong = 0
+    , queue = Array.empty
+    , files = [{path = "/home/eliot/Music/Micachu%20and%20the%20Shapes%20-%20Good%20Sad%20Happy%20Bad%20%282015%29%20-%20CD%20V0/10%20Peach.mp3", name = "10 Peach.mp3"}]
+    }
+
+
+
+-- UPDATE
+
+
+type Msg
+    = ClickFile FileRecord
+    | NextSong
+
+
+update : Msg -> Model -> Model
+update action model =
+    case action of
+        ClickFile fileRecord ->
+            { model
+                | queue = Array.push fileRecord model.queue
+            }
+
+        NextSong ->
+            let
+                shouldReset =
+                    ((Array.length model.queue) - 1) <= Debug.log "currentSong" model.currentSong
+            in
+                if shouldReset then
+                    { model
+                        | currentSong = Debug.log "NextSongRESET" 0
+                    }
+                else
+                    { model
+                        | currentSong = Debug.log "NextSong" <| (model.currentSong + 1)
+                    }
+
+
+-- SUBSCRIPTIONS
+-- subscriptions : Model -> Sub Msg
+-- subscriptions model =
+-- VIEW
+
+
+view : Model -> Html Msg
+view model =
+    Html.div []
+        [ fileView model.files
+        , queueView model.queue
+        , audioPlayer model
+        ]
+
+
+audioPlayer : Model -> Html Msg
+audioPlayer model =
+    case Debug.log "new player" (Array.get model.currentSong model.queue) of
+        Just fileRecord ->
+            let
+                srcWithFile =
+                    "file://" ++ fileRecord.path
+            in
+                Html.audio
+                    [ Attr.src srcWithFile
+                    , Attr.type' "audio/mp3"
+                    , Attr.controls True
+                    , Attr.autoplay True
+                    , Events.on "ended" (JsonD.succeed NextSong)
+                    ]
+                    []
+
+        Nothing ->
+            Html.div [ ] [ Html.text "------------- \x08\n                                                               -------------- \n Nothing playing" ]
+
+
+fileView : List FileRecord -> Html Msg
+fileView files =
+    Html.div [ songList ] <| List.map fileToHtml files
+
+
+fileToHtml : FileRecord -> Html Msg
+fileToHtml file =
+    Html.div [ Events.onClick <| ClickFile file ] [ Html.text file.name ]
+
+
+queueView : Array FileRecord -> Html Msg
+queueView files =
+    Html.div []
+        (List.map (Html.text << .name)
+            <| Array.toList files
+        )
+
+
+
+-- songList : Attribute msg
+
+
+songList =
+    Attr.style
+        [ ( "width", "50%" )
+        , ( "float", "left" )
+        ]
